d75a84e5f8f46b8bf46aa0148d6c2eb065ff9d81
Ed Carroll <e.carroll.code@gmail.com>
2020-01-11 16:30:41 -0800

    add ArcDSB windows service for adding features to geonet

---
+++ ARAppSourceCode/ArcDSB/GeoEventDataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using QuickType;
+
+namespace ArcDSB
+{
+    public partial class GeoEventDataService : ServiceBase
+    {
+        public GeoEventDataService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string json_raw = "";
+            try
+            {
+                json_raw = new System.Net.WebClient().DownloadString("https://opensky-network.org/api/states/all?lamin=33.87845&lomin=-117.81135&lamax=34.28221&lomax=-116.94345");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            State[] states = JSONData.FromJson(json_raw).States;
+            foreach (var state in states)
+            {
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
