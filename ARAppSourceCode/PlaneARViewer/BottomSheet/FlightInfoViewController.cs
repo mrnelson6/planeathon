35355f7ea84fde2843eefe89f69f128c7dfa1eb4
Nathan Castle <ncastle@esri.com>
2020-01-12 14:00:34 -0800

    Enable live updates

--- ARAppSourceCode/PlaneARViewer/BottomSheet/FlightInfoViewController.cs
+++ ARAppSourceCode/PlaneARViewer/BottomSheet/FlightInfoViewController.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using SharedAirplaneFinder;
 using UIKit;
 
 namespace PlaneARViewer.BottomSheet
 {
     internal class FlightInfoViewController : UITableViewController
     {
+        private AirplaneFinder _airplaneFinder;
+        private FlightInfoViewControllerDataSource _dataSource;
+
         public FlightInfoViewController() : base()
         {
-            TableView.DataSource = new FlightInfoViewControllerDataSource();
+            _dataSource = new FlightInfoViewControllerDataSource();
+            TableView.DataSource = _dataSource;
             TableView.AllowsSelection = false;
             TableView.ScrollEnabled = false;
 
             View.Layer.CornerRadius = 16;
             View.Layer.Opacity = 0.9f;
+        }
+
+        public void AssociateAirplaneFinder(AirplaneFinder airplaneFinder)
+        {
+            _airplaneFinder = airplaneFinder;
+
+            _airplaneFinder.PropertyChanged += _airplaneFinder_PropertyChanged;
+        }
+
+        private void _airplaneFinder_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(AirplaneFinder.SelectedPlane))
+            {
+                _dataSource.Update(_airplaneFinder.SelectedPlane);
+            }
         }
 
         public int GetViewHeight() => 64 * 3 + FlightHeaderViewCell.Height;
